5c32c8965879d4be18f48cb404fc5b11e9613d5d
Viktor Verebelyi <vviktor2@gmail.com>
2017-01-19 05:02:13 +0000

    Little housekeeping

--- main.cpp
+++ main.cpp
@@ -20,56 +20,20 @@ int main(int argc, char *argv[])
 
     QGst::init(&argc, &argv);
 
-    QJsonObject menuItemAC
-        {
-            {"source","qrc:/qml/ClimateControl/CCLayout.qml"},
-            {"image","icons/svg/thermometer.svg"},
-            {"text","A/C"},
-            {"color","#d32f2f"}
-        };
-
-    QJsonObject menuItemSettings
-        {
-            {"source","Item"},
-            {"image","icons/gear-a.png"},
-            {"text","Settings"},
-            {"color","#fbc02d"}
-        };
-
-    QJsonObject menuItemRadio
-        {
-            {"source","qrc:/qml/Radio/RadioLayout.qml"},
-            {"image","icons/svg/radio-waves.svg"},
-            {"text","Radio"},
-            {"color","#512da8"}
-        };
-
-    QJsonObject menuItemAA
-        {
-            {"source","qrc:/aaVideo.qml"},
-            {"image","icons/svg/social-android.svg"},
-            {"text","Android Auto"},
-            {"color","#388e3c"}
-        };
-
-    QJsonObject menuItemMusic
-        {
-            {"source","Item"},
-            {"image","icons/svg/social-note.svg"},
-            {"text","ASdsd"},
-            {"color","#0288d1"}
-        };
-
     QVariantList menuItems;
-    menuItems << menuItemAC.toVariantMap() << menuItemSettings.toVariantMap() << menuItemRadio.toVariantMap() << menuItemAA.toVariantMap() << menuItemMusic.toVariantMap();
-    QGst::Quick::VideoSurface *surface = new QGst::Quick::VideoSurface;
-    Headunit *headunit = new Headunit(surface->videoSink());
-    engine.rootContext()->setContextProperty("videoSurface1", surface);
-    engine.rootContext()->setContextProperty("headunit", headunit);
+    menuItems << QJsonObject {{"source","qrc:/qml/ClimateControl/CCLayout.qml"},{"image","icons/svg/thermometer.svg"},{"text","A/C"},{"color","#d32f2f"}}.toVariantMap()
+              << QJsonObject {{"source","Item"},{"image","icons/gear-a.png"},{"text","Settings"},{"color","#fbc02d"}}.toVariantMap()
+              << QJsonObject {{"source","qrc:/qml/Radio/RadioLayout.qml"},{"image","icons/svg/radio-waves.svg"},{"text","Radio"},{"color","#512da8"}}.toVariantMap()
+              << QJsonObject {{"source","qrc:/aaVideo.qml"},{"image","icons/svg/social-android.svg"},{"text","Android Auto"},{"color","#388e3c"}}.toVariantMap()
+              << QJsonObject {{"source","Item"},{"image","icons/svg/music-note.svg"},{"text","ASdsd"},{"color","#0288d1"}}.toVariantMap();
+    QGst::Quick::VideoSurface surface;
+    Headunit headunit(surface.videoSink());
+    engine.rootContext()->setContextProperty("videoSurface1", &surface);
+    engine.rootContext()->setContextProperty("headunit", &headunit);
     engine.rootContext()->setContextProperty("menuItems", menuItems);
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
-    app.exec();
-    headunit->stop();
-    return 0;
+    int ret = app.exec();
+    headunit.stop();
+    return ret;
 }
