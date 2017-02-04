db16fa0e62302d48acbbccfd5f91159762f96f73
Viktor Verebelyi <vviktor2@gmail.com>
2017-02-04 01:40:56 +0000

    Added MediaLibrary class This class is responsible for handling date between the mediascanner and the GUI

--- medialibrary.cpp
+++ medialibrary.cpp
@@ -2,5 +2,21 @@
 
 MediaLibrary::MediaLibrary(QObject *parent) : QObject(parent)
 {
-
+    mediaScanner = new MediaScanner();
+}
+const QVariantList MediaLibrary::audioFolders() {
+    if(p_audioFolders.isEmpty())
+        p_audioFolders = mediaScanner->mediadb->getMediaFolders(MediaDB::AUDIO);
+    return p_audioFolders;
+}
+const QVariantList MediaLibrary::playlists() {
+    if(p_playlists.isEmpty())
+        p_playlists = mediaScanner->mediadb->getPlaylists();
+    return p_playlists;
+}
+QVariantList MediaLibrary::audioFolderContent(int folder_id) {
+    return mediaScanner->mediadb->getFolderContent(folder_id, MediaDB::AUDIO);
+}
+QVariantList MediaLibrary::videoFolderContent(int folder_id) {
+    return mediaScanner->mediadb->getFolderContent(folder_id, MediaDB::VIDEO);
 }
--- medialibrary.h
+++ medialibrary.h
@@ -2,13 +2,26 @@
 #define MEDIALIBRARY_H
 
 #include <QObject>
+#include <QQmlListProperty>
+#include <QVariant>
 
+#include "mediascanner.h"
+#include "mediadb.h"
 class MediaLibrary : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QVariantList audioFolders READ audioFolders CONSTANT)
+    Q_PROPERTY(QVariantList playlists READ playlists CONSTANT)
 public:
     explicit MediaLibrary(QObject *parent = 0);
-
+    const QVariantList audioFolders();
+    const QVariantList playlists();
+    Q_INVOKABLE QVariantList audioFolderContent(int folder_id);
+    Q_INVOKABLE QVariantList videoFolderContent(int folder_id);
+private:
+    MediaScanner *mediaScanner;
+    QVariantList p_audioFolders;
+    QVariantList p_playlists;
 signals:
 
 public slots:
