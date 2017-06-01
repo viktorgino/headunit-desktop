aefdcebb3eb8081a9925d7971f8175b8e4acaedf
Viktor Verebelyi <vviktor2@gmail.com>
2017-06-01 05:03:39 +0100

    Add error handling to welle.io code, remove RTL_TCP and RAW file code

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <QThreadPool>
 #include <QDir>
 #include <QSettings>
+#include <QtDebug>
 #include <unistd.h>
 #include "headunit.h"
 #include "usbconnectionlistener.h"
@@ -38,22 +39,21 @@ int main(int argc, char *argv[])
 
     QGst::init(&argc, &argv);
 
+
     int defaultMenuItem = 2;
     QVariantList menuItems;
     menuItems << QJsonObject {{"source","qrc:/qml/ClimateControl/CCLayout.qml"},{"image","icons/svg/thermometer.svg"},{"text","A/C"},{"color","#f44336"}}.toVariantMap()
               << QJsonObject {{"source","qrc:/qml/Radio/RadioLayout.qml"},{"image","icons/svg/radio-waves.svg"},{"text","Radio"},{"color","#E91E63"}}.toVariantMap()
-             #ifdef HAVE_WELLEIO
-              << QJsonObject {{"source","qrc:/DABLayout.qml"},{"image","icons/svg/radio-waves.svg"},{"text","DAB"},{"color","#9C27B0"}}.toVariantMap()
-             #endif
               << QJsonObject {{"source","qrc:/aaVideo.qml"},{"image","icons/svg/social-android.svg"},{"text","Android Auto"},{"color","#673AB7"}}.toVariantMap()
               << QJsonObject {{"source","qrc:/qml/MediaPlayer/MediaPlayerLayout.qml"},{"image","icons/svg/music-note.svg"},{"text","Media player"},{"color","#3F51B5"}}.toVariantMap()
               << QJsonObject {{"source","qrc:/qml/SettingsPage/SettingsPage.qml"},{"image","icons/gear-a.png"},{"text","Settings"},{"color","#2196F3"}}.toVariantMap();
     QGst::Quick::VideoSurface surface;
     Headunit *headunit =  new Headunit(surface.videoSink());
     MediaLibrary *mediaLibrary = new MediaLibrary();
+
     //Load welle.io
 #ifdef HAVE_WELLEIO
-
+    bool welleioError = false;
     // Default values
     CDABParams DABParams(1);
     QString dabDevice = "auto";
@@ -63,33 +63,38 @@ int main(int argc, char *argv[])
     QString rawFileFormat = "u8";
 
     // Init device
-    CVirtualInput* Device = CInputFactory::GetDevice(dabDevice);
-
-    // Set rtl_tcp settings
-    if (Device->getID() == CDeviceID::RTL_TCP) {
-        CRTL_TCP_Client* RTL_TCP_Client = (CRTL_TCP_Client*)Device;
-
-        RTL_TCP_Client->setIP(ipAddress);
-        RTL_TCP_Client->setPort(ipPort);
+    CVirtualInput* Device;
+    try {
+        Device = CInputFactory::GetDevice(dabDevice);
+    } catch (int e) {
+        qDebug()<< "DAB device not found! ";
+        welleioError = true;
     }
+    CRadioController* RadioController;
+    CGUI *GUI;
+    if(!welleioError){
+        try {
 
-    // Set rawfile settings
-    if (Device->getID() == CDeviceID::RAWFILE) {
-        CRAWFile* RAWFile = (CRAWFile*)Device;
-
-        RAWFile->setFileName(rawFile, rawFileFormat);
+            // Create a new radio interface instance
+            RadioController = new CRadioController(Device, DABParams);
+            GUI = new CGUI(RadioController, &DABParams);
+            // Load welle.io's control object to QML context
+            engine.rootContext()->setContextProperty("cppGUI", GUI);
+            // Add MOT slideshow provider
+            engine.addImageProvider(QLatin1String("motslideshow"), GUI->MOTImage);
+            // Load the DAB helper class to QML context
+            DABHelper *dabHelper = new DABHelper();
+            engine.rootContext()->setContextProperty("dabHelper", dabHelper);
+        } catch (int e) {
+            qDebug()<< "Can't initialise welle.io. Exception: " << e ;
+            welleioError = true;
+        }
     }
-    // Create a new radio interface instance
-    CRadioController* RadioController = new CRadioController(Device, DABParams);
-    CGUI *GUI = new CGUI(RadioController, &DABParams);
-    // Load welle.io's control object to QML context
-    engine.rootContext()->setContextProperty("cppGUI", GUI);
-    // Add MOT slideshow provider
-    engine.addImageProvider(QLatin1String("motslideshow"), GUI->MOTImage);
-    // Load the DAB helper class to QML context
-    DABHelper *dabHelper = new DABHelper();
-    engine.rootContext()->setContextProperty("dabHelper", dabHelper);
+    if(!welleioError){
+        menuItems.insert(2,QJsonObject {{"source","qrc:/DABLayout.qml"},{"image","icons/svg/radio-waves.svg"},{"text","DAB"},{"color","#9C27B0"}}.toVariantMap());
+    }
 #endif
+
     engine.rootContext()->setContextProperty("videoSurface1", &surface);
     engine.rootContext()->setContextProperty("headunit", headunit);
     engine.rootContext()->setContextProperty("menuItems", menuItems);
@@ -103,8 +108,10 @@ int main(int argc, char *argv[])
 
     int ret = app.exec();
 #ifdef HAVE_WELLEIO
-    delete GUI;
-    delete RadioController;
+    if(!welleioError){
+        delete GUI;
+        delete RadioController;
+    }
 #endif
     delete(headunit);
     connectionListener->stop();
