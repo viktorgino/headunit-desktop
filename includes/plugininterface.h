ced64470d4364d458586594fd62f8a824bcc27b8
viktorgino <vviktor2@gmail.com>
2020-02-07 00:27:30 +0000

    [Plugininterface]

--- includes/plugininterface.h
+++ includes/plugininterface.h
@@ -16,8 +16,8 @@ public:
     virtual QStringList eventListeners() = 0;
     virtual QStringList events() = 0;
     virtual QStringList actions() = 0;
-    virtual ~PluginInterface() = 0;
-    virtual void onLoad() {};
+    virtual ~PluginInterface() {}
+    virtual void onLoad() {}
     QQmlPropertyMap settings;
 public slots:
     virtual void eventMessage(QString id, QString message) = 0;
